67a2101c532de357a88c4d837fd13ab08c036bb1
karimSOV <karim.sovero@tecsup.edu.pe>
2026-08-26 00:50:08 -0500

    final sync sprint-1

--- src/StudentController.java
+++ src/StudentController.java
@@ -3,6 +3,7 @@ public class StudentController{
     public void controller(){
         System.out.println("Student Controller running");
         System.out.println("Sprint 1 update");
+        System.out.println("Final sync check");
     }
 
 }
