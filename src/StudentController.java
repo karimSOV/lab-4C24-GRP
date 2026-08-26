1040320cfaab8a4d57075f8bbceb6aebd2c78e50
karimSOV <karim.sovero@tecsup.edu.pe>
2026-08-26 00:10:35 -0500

    Update SutdentController on sprint-1

--- src/StudentController.java
+++ src/StudentController.java
@@ -2,6 +2,7 @@ public class StudentController{
 
     public void controller(){
         System.out.println("Student Controller running");
+        System.out.println("Sprint 1 update");
     }
 
 }
